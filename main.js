06e36849b1f519014b1247d97c2a26ccf69aad46
jonmatthis <jonmatthis@gmail.com>
2024-04-05 08:30:25 -0400

    starting mdn tut

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
 import * as THREE from 'three';
 
-const mainScene = new THREE.Scene();
-
-const mainCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
 
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement); 
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xdddddd, 1);
+document.body.appendChild(renderer.domElement);
 
-const torusGeometry = new THREE.TorusKnotGeometry(1, 0.4, 100, 16);
+const scene = new THREE.Scene();
 
-const greenMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const torusKnotMesh = new THREE.Mesh(torusGeometry, greenMaterial);
-
-mainScene.add(torusKnotMesh);
-mainCamera.position.z = 5;
-
-function animate() {
-    requestAnimationFrame(animate); 
-    torusKnotMesh.rotation.x += 0.01;
-    torusKnotMesh.rotation.y += 0.01;
-    torusKnotMesh.rotation.z += 0.01;
-    renderer.render(mainScene, mainCamera);
-}
-animate();
+const camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT);
+camera.position.z = 50;
+scene.add(camera);
+THREE.CubeCamera
